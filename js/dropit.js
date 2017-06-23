6499e145d031c8884f51ddbe4193189cf037eff2
Tabr0793 <tbrown4740@gmail.com>
2017-06-23 13:42:16 -0700

    dashboard image updated

--- js/dropit.js
+++ js/dropit.js
@@ -21,28 +21,28 @@
                          settings = $.fn.dropit.settings;
 
                     // Hide initial submenus
-                    $el.addClass('dropit dropup')
-                    .find('>'+ settings.triggerParentEl +':has('+ settings.submenuEl +')').addClass('dropit-trigger dropup-trigger')
+                    $el.addClass('dropit')
+                    .find('>'+ settings.triggerParentEl +':has('+ settings.submenuEl +')').addClass('dropit-trigger')
                     .find(settings.submenuEl).addClass('dropit-submenu').hide();
 
                     // Open on click
                     $el.off(settings.action).on(settings.action, settings.triggerParentEl +':has('+ settings.submenuEl +') > '+ settings.triggerEl +'', function(){
                         // Close click menu's if clicked again
-                        if(settings.action == 'click' && $(this).parents(settings.triggerParentEl).hasClass('dropit-open dropup-open')){
+                        if(settings.action == 'click' && $(this).parents(settings.triggerParentEl).hasClass('dropit-open')){
                             settings.beforeHide.call(this);
-                            $(this).parents(settings.triggerParentEl).removeClass('dropit-open dropup-open').find(settings.submenuEl).hide();
+                            $(this).parents(settings.triggerParentEl).removeClass('dropit-open').find(settings.submenuEl).hide();
                             settings.afterHide.call(this);
                             return false;
                         }
 
                         // Hide open menus
                         settings.beforeHide.call(this);
-                        $('.dropit-open dropup-open').removeClass('dropit-open dropup-open').find('.dropit-submenu dropup-submenu').hide();
+                        $('.dropit-open').removeClass('dropit-open').find('.dropit-submenu').hide();
                         settings.afterHide.call(this);
 
                         // Open this menu
                         settings.beforeShow.call(this);
-                        $(this).parents(settings.triggerParentEl).addClass('dropit-open dropup-open').find(settings.submenuEl).show();
+                        $(this).parents(settings.triggerParentEl).addClass('dropit-open').find(settings.submenuEl).show();
                         settings.afterShow.call(this);
 
                         return false;
@@ -51,15 +51,15 @@
                     // Close if outside click
                     $(document).on('click', function(){
                         settings.beforeHide.call(this);
-                        $('.dropit-open dropup-open').removeClass('dropit-open dropup-open').find('.dropit-submenu dropup-submenu').hide();
+                        $('.dropit-open').removeClass('dropit-open').find('.dropit-submenu').hide();
                         settings.afterHide.call(this);
                     });
 
                     // If hover
                     if(settings.action == 'mouseenter'){
-                        $el.on('mouseleave', '.dropit-open dropup-open', function(){
+                        $el.on('mouseleave', '.dropit-open', function(){
                             settings.beforeHide.call(this);
-                            $(this).removeClass('dropit-open dropup-open').find(settings.submenuEl).hide();
+                            $(this).removeClass('dropit-open').find(settings.submenuEl).hide();
                             settings.afterHide.call(this);
                         });
                     }
@@ -91,8 +91,12 @@
         beforeHide: function(){}, // Triggers before submenu is hidden
         afterHide: function(){} // Triggers before submenu is hidden
     };
+// When the user clicks on <div>, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
 
     $.fn.dropit.settings = {};
-    $.fn.dropup.settings = {};
 
 })(jQuery);
